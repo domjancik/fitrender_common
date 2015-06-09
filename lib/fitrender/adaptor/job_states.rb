7cdb45166224bccec93bbe9a94e47231043c001d
Dominik Jancik <jancidom@fit.cvut.cz>
2015-06-09 14:44:55 +0200

    Changed order of job states

--- lib/fitrender/adaptor/job_states.rb
+++ lib/fitrender/adaptor/job_states.rb
@@ -1,18 +1,18 @@
 module Fitrender
   module Adaptor
     module States
+      JOB_STATE_OTHER = 'other'
       JOB_STATE_IDLE = 'idle'
       JOB_STATE_RUNNING = 'running'
       JOB_STATE_COMPLETED = 'completed'
       JOB_STATE_FAILED = 'failed'
-      JOB_STATE_OTHER = 'other'
 
       JOB_STATES = [
+        JOB_STATE_OTHER,
         JOB_STATE_IDLE,
         JOB_STATE_RUNNING,
         JOB_STATE_COMPLETED,
-        JOB_STATE_FAILED,
-        JOB_STATE_OTHER
+        JOB_STATE_FAILED
       ]
     end
   end
